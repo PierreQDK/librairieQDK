9cee6bab8b5b5b93c000c0393bb02d87738cb1c5
PierreQDK <pierre.quintindekercadio@gmail.com>
2025-03-22 17:50:16 +0100

    Update Importdonnees.R

--- R/Importdonnees.R
+++ R/Importdonnees.R
@@ -1,6 +1,6 @@
 library(usethis)
 
-elus_data <- readr::read_delim("/Users/pierrequintindekercadio/Documents/Documents - Mac/elus_sample.csv")
+elus_data <- readr::read_delim("data/elus_sample.csv")
 
 usethis::use_data(elus_data, overwrite = TRUE)
 
